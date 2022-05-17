a6d1dd93d4dc7b6017f7b8239d43ea91b03ef6d0
Vladyslav Komar <valdek125@gmail.com>
2022-05-17 03:56:05 +0300

    таск 6 видалив консоль логи

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,7 @@ const inputText = document.querySelector("#validation-input")
 const isValid = (e) => {
     const inputLength = inputText.value.length
     const dataLength = inputText.dataset.length
-    console.log(inputLength)
-     console.log(dataLength)
-    // inputText.classList.add(inputLength === dataLength ? "valid" : "invalid")
+    
     if (Number(inputLength) === Number(dataLength) ) {
         inputText.classList.add("valid")
         inputText.classList.remove("invalid")
